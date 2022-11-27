29f2acc088511876ddbebdc44945efe1c33a44e6
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2022-11-27 10:09:13 +0530

    Create 08 Factorial of a numbet using recursion.c

---
+++ recursion/08 Factorial of a numbet using recursion.c
@@ -0,0 +1,23 @@
+/* Factorial of a numbet using recursion 
+code 
+
+*/
+
+# include <stdio.h>
+int fact(int n)
+{
+	if (n==0)
+	return 1;
+	else 
+	return fact(n-1)*n;
+}
+int main ()
+{
+	int x;
+	printf("Enter a number ");
+	scanf("%d",&x);
+	int a=fact(x);
+	printf("The factorail of given number is %d : ",a);
+	
+	return 0;
+}
