f48a3126eb66e5b7ef109b23ef7ff60765cc1b3d
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2022-11-28 10:10:48 +0530

    Create 10 powe using recursion.cpp

---
+++ recursion/10 powe using recursion.cpp
@@ -0,0 +1,24 @@
+/* Power Using Recursion
+
+*/
+
+#include<stdio.h>
+#include<math.h>
+int power(m,n)
+{
+	if (m==0)
+	return 1;
+	else 
+	return pow(m,n-1)*m;
+	
+}
+int main ()
+{
+	int a,b;
+	printf("enter the numbers ");
+	scanf("%d %d",&a,&b);
+	int c=power(a,b);
+	printf("%d",c);
+	return 0;
+	
+}
