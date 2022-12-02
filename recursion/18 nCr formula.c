ca54370eb67f229defcb80ececcbacac16dfe326
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2022-12-02 15:00:53 +0530

    Create 18 nCr formula.c

---
+++ recursion/18 nCr formula.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int fact(int n)
+{
+	if (n<=1)
+	return 1;
+	else 
+	return fact(n-1)*n;
+}
+int nCr(int n,int r)
+
+
+{
+	int num,den;
+	num= fact(n);
+	den=fact(r)*fact(n-r);
+	
+	return num/den;
+}
+int main ()
+{
+	int x,y;
+	printf("Enter the value of n and r  \n");
+	scanf("%d%d",&x,&y);
+	printf("The nCr is : %d",nCr(x,y));
+}
