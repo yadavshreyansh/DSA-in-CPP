68c40b50793f03b2412c9dfbbc15d3f1817fec76
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-01-31 17:50:15 +0530

    Create Minimum times A has to be repeated such that B is a substring of it.cpp

---
+++ string/Minimum times A has to be repeated such that B is a substring of it.cpp
@@ -0,0 +1,41 @@
+//User function Template for C++
+
+class Solution {
+
+  public:
+
+    int minRepeats(string A, string B) {
+
+        // code here
+
+        string temp=A;
+
+        int moves=1;
+
+        while(A.size()<B.size())
+
+        {
+
+            A+=temp;
+
+            moves++;
+
+        }
+
+        if(A.find(B)!=-1)return moves;
+
+        
+
+        A+=temp;
+
+        moves++;
+
+        if(A.find(B)!=-1)return moves;
+
+        
+
+        return -1;
+
+    }
+
+};
